cfa62412399d1e1d2b20e20a9d274402a4222240
Noirrr <sinsenju537@gmail.com>
2024-07-13 13:07:48 +0700

    mengintegrasikan midtrans

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
 const { PrismaClient } = require("@prisma/client");
+const midtransClient = require("midtrans-client");
 
 const prisma = new PrismaClient();
 
@@ -17,7 +18,7 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/product", async (req, res) => {
-  const product = await prisma.findMany();
+  const product = await prisma.product.findMany();
   res.send(product);
 });
 app.post("/product", async (req, res) => {
@@ -39,6 +40,7 @@ app.get("/CART", async (req, res) => {
   const sendProduct = await prisma.productCart.findMany();
   res.send(sendProduct);
 });
+
 app.get("/CART/:id", async (req, res) => {
   const productId = req.params.id;
   const sendProduct = await prisma.productCart.findUnique({
@@ -120,6 +122,47 @@ app.delete("/CART/:id", async (req, res) => {
       success: false,
       message: "Failed to fetch delete products from cart",
       error: error,
+    });
+  }
+});
+
+let snap = new midtransClient.Snap({
+  isProduction: false,
+  serverKey: process.env.SERVER_KEY,
+  clientKey: process.env.CLIENT_KEY,
+});
+
+app.post("/CART/BUY", async (req, res) => {
+  const buyingProduct = req.body;
+  try {
+    let parameter = {
+      transaction_details: {
+        order_id: Number(
+          Date.now().toString() +
+            Math.floor(Math.random() * 1000) +
+            buyingProduct.id
+        ),
+        gross_amount: buyingProduct.price * buyingProduct.qty,
+      },
+    };
+    console.log(parameter);
+    console.log(buyingProduct);
+    const token = await snap.createTransactionToken(parameter);
+
+    res.send({
+      success: true,
+      token: token,
+      message: "snap open",
+    });
+  } catch (error) {
+    const simpleError = JSON.stringify(
+      error,
+      Object.getOwnPropertyNames(error)
+    ); // Remove circular structure
+    res.send({
+      success: false,
+      error: simpleError,
+      message: error.message,
     });
   }
 });
