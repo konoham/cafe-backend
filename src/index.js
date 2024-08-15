a67e269b61b101d01b5b963d1fa0c4b82abbdfbd
RABANI <sinsenju537@gmail.com>
2024-08-15 20:23:13 +0700

    memperbaiki api

--- src/index.js
+++ src/index.js
@@ -17,50 +17,27 @@ app.get("/", async (req, res) => {
   res.send("UTAMA");
 });
 
-app.get("/product", async (req, res) => {
-  const product = await prisma.product.findMany();
-  res.send(product);
-});
-app.post("/product", async (req, res) => {
-  const data = req.body;
-
-  const sendProduct = await prisma.create({
-    data: {
-      name: data.name,
-      price: data.price,
-      images: data.images,
-    },
-  });
-  res.status(200).send({
-    data: sendProduct,
-    message: "send product succses",
-  });
-});
-app.get("/CART", async (req, res) => {
-  const sendProduct = await prisma.productCart.findMany();
-  res.send(sendProduct);
-});
-
-app.get("/CART/:id", async (req, res) => {
-  const productId = req.params.id;
-  const sendProduct = await prisma.productCart.findUnique({
+app.get("/CART/:email", async (req, res) => {
+  const { email } = req.params;
+  const sendProduct = await prisma.productCart.findMany({
     where: {
-      id: parseInt(productId),
+      email,
     },
   });
   res.send(sendProduct);
 });
+
 app.post("/CART", async (req, res) => {
-  const dataProduct = req.body;
+  const { name, price, images, email, country, qty } = req.body;
   try {
     const sendProduct = await prisma.productCart.create({
       data: {
-        name: dataProduct.name,
-        price: dataProduct.price,
-        images: dataProduct.images,
-        email: dataProduct.email,
-        country: dataProduct.country,
-        qty: dataProduct.qty,
+        name,
+        price,
+        images,
+        email,
+        country,
+        qty,
       },
     });
 
@@ -73,24 +50,25 @@ app.post("/CART", async (req, res) => {
       success: false,
       message: "Failed to fetch cart products",
       error: error.meta.target,
-      data: dataProduct,
+      data: req.body,
     });
   }
 });
+
 app.patch("/CART/:id", async (req, res) => {
-  const productId = parseInt(req.params.id);
-  const dataProduct = req.body;
+  const id = parseInt(req.params.id);
+  const { qty } = req.body;
+  console.log({ qty, id });
+
   try {
     const sendProduct = await prisma.productCart.update({
       where: {
-        id: productId,
+        id,
       },
       data: {
-        qty: dataProduct.qty,
+        qty,
       },
     });
-    console.log(productId);
-    console.log(dataProduct);
 
     res.status(200).json({
       success: true,
@@ -105,6 +83,7 @@ app.patch("/CART/:id", async (req, res) => {
     });
   }
 });
+
 app.delete("/CART/:id", async (req, res) => {
   const productId = parseInt(req.params.id);
   try {
@@ -137,17 +116,13 @@ app.post("/CART/BUY", async (req, res) => {
   try {
     let parameter = {
       transaction_details: {
-        order_id: Number(
-          Date.now().toString() +
-            Math.floor(Math.random() * 1000) +
-            buyingProduct.id
-        ),
+        order_id: Number(Date.now().toString() + Math.floor(Math.random() * 1000) + buyingProduct.id),
         gross_amount: buyingProduct.price * buyingProduct.qty,
       },
     };
     console.log(parameter);
-    console.log(buyingProduct);
     const token = await snap.createTransactionToken(parameter);
+    console.log(token);
 
     res.send({
       success: true,
@@ -155,10 +130,7 @@ app.post("/CART/BUY", async (req, res) => {
       message: "snap open",
     });
   } catch (error) {
-    const simpleError = JSON.stringify(
-      error,
-      Object.getOwnPropertyNames(error)
-    ); // Remove circular structure
+    const simpleError = JSON.stringify(error, Object.getOwnPropertyNames(error)); // Remove circular structure
     res.send({
       success: false,
       error: simpleError,
